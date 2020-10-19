03f2dee6ae5c104fb9b216d1859f14feac4a9466
tappictivestudios2020 <70457408+tappictivestudios2020@users.noreply.github.com>
2020-10-19 12:34:53 +1100

    Player Movement & Camera follow player
    Player movement script and camera follow player has been implemented.
    Next version will be working on better gameplay experience such as camera movement, player movement ...

---
+++ NoLight/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    [SerializeField]private Vector3 offset;
+    //private float smoothTime = 10f;
+
+    private void FixedUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 playerPosition = player.position + offset;
+        //Vector3 smoothCamera = Vector3.Lerp(transform.position,playerPosition,smoothTime*Time.fixedDeltaTime);
+        transform.position = playerPosition;
+    }
+
+}
--- NoLight/Assets/Scripts/PlayerMovement.cs
+++ NoLight/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Variables
+    private float speed = 5f;
+    private float jumpForce = 5;
+    Rigidbody2D body;
+
+    public bool isGrounded;
+    public LayerMask groundLayer;
+
+    private Collider2D characterCollider;
+
+    private float offEdgeTime = 0.1f;
+    private float offEdegeCounter;
+
+    float jumpBufferTime = 0.1f,jumpBufferCounter;
+
+
+    float movement;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        characterCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics2D.IsTouchingLayers(characterCollider, groundLayer);
+
+        //off edge jump 
+        if (isGrounded)
+        {
+            offEdegeCounter = offEdgeTime;
+        }
+        else
+        {
+            offEdegeCounter -= Time.deltaTime;
+        }
+
+        //jump buffer, allow the player to jump again in 0.1sec before they reach the ground.
+        if (Input.GetButton("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        movement = Input.GetAxis("Horizontal");
+
+        if (jumpBufferCounter >= 0 && offEdegeCounter > 0f)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+            jumpBufferCounter = 0;
+        }
+        if(Input.GetButtonUp("Jump")&& body.velocity.y > 0)
+        {   //if the player hold down the space bar then they can jump a little bit higher.
+            body.velocity = new Vector2(body.velocity.x, body.velocity.y * .5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
     }
 }
